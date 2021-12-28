17493f8599a75b05636d15300938cb44b614e9c9
Sai Praneeth S <sai251180@gmail.com>
2021-12-28 10:20:27 +0000

    Update solution.sql

--- Lyft/Distances Traveled/solution.sql
+++ Lyft/Distances Traveled/solution.sql
@@ -1,1 +1,11 @@
 
+SELECT 
+ lu.id,
+ lu.name,
+ SUM(lg.distance) AS total_distance
+FROM lyft_rides_log lg
+JOIN lyft_users lu
+ON lg.user_id = lu.id
+GROUP BY lu.id,lu.name
+ORDER BY total_distance DESC
+LIMIT 10;
